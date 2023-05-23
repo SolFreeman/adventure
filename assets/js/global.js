2781ea4ead395fa748d6656a139212fe76650d9a
SolFreeman <soloviyov.stas@gmail.com>
2023-05-23 19:57:11 +0300

    Updates

--- assets/js/global.js
+++ assets/js/global.js
@@ -8,7 +8,7 @@ $(window).on('load resize', function () {
 			arrows: false
 		});
 
-		$('.secrets-slider:not(.slick-initialized)').slick({
+		$('.js-secrets-slider:not(.slick-initialized)').slick({
 			dots: true,
 			infinite: true,
 			speed: 200,
@@ -16,18 +16,18 @@ $(window).on('load resize', function () {
 			arrows: false
 		});
 
-		$('.search-list:not(.slick-initialized)').slick({
-			dots: true,
-			infinite: true,
-			speed: 200,
-			slidesToShow: 1,
-			arrows: false
-		});
+		// $('.search-list:not(.slick-initialized)').slick({
+		// 	dots: true,
+		// 	infinite: true,
+		// 	speed: 200,
+		// 	slidesToShow: 1,
+		// 	arrows: false
+		// });
 
 	} else {
-		$(".secrets-slider.slick-initialized").slick("unslick");
+		$(".js-secrets-slider.slick-initialized").slick("unslick");
 		$(".review-slider.slick-initialized").slick("unslick");
-		$(".search-list.slick-initialized").slick("unslick");
+		// $(".search-list.slick-initialized").slick("unslick");
 	}
 
 	if ($(window).width() < 1340) {
@@ -37,13 +37,8 @@ $(window).on('load resize', function () {
 
 jQuery(document).ready(function ($) {
 	faqAccordeon();
-	inputFileCustom();
-	listCategory();
-	tooltip();
 	$(".fancybox").fancybox();
 	openDropdown();
-	resizeBlock();
-	subMenuNavigation();
 	addClass();
 
 	$(window).on('beforeunload', function () {
@@ -62,9 +57,14 @@ jQuery(document).ready(function ($) {
 		theme: 'single'
 	});
 	modalOpen();
-	if ($(window).width() < 768) {
-		loadMore();
-	}
+
+	$('.form-box .text-input').on('input', function () {
+		if ($(this).val().trim() !== '') {
+			$(this).addClass('filled');
+		} else {
+			$(this).removeClass('filled');
+		}
+	});
 
 	$('ul.tabs__caption').on('click', 'li:not(.active)', function () {
 		$(this)
@@ -198,12 +198,14 @@ jQuery(document).ready(function ($) {
 	$('.happy-slider').slick({
 		arrows: true,
 		dots: true,
-		slidesToShow: 1,
+		slidesToShow: 3,
+		variableWidth: true,
 		responsive: [
 			{
 				breakpoint: 768,
 				settings: {
-					arrows: false
+					arrows: false,
+					slidesToShow: 3
 				},
 			}
 		]
@@ -286,76 +288,9 @@ jQuery(document).ready(function ($) {
 });
 
 
-function inputFileCustom() {
-	// Список всех <input> на странице
-	const inputs = document.querySelectorAll('input[type="file"]');
-
-	// Перебираем все <input>
-	inputs.forEach(input => {
-		// Блок предпросмотра
-		const preview = input.closest('.calculator__form').querySelector('.preview');
-
-		// Список файлов
-		const fileList = [];
-
-		// Вешаем функцию onChange на событие change у <input>
-		input.addEventListener('change', onChange);
-
-		function onChange() {
-			// По каждому файлу <input>
-			[...input.files].forEach(file => {
-				// Создаём читателя
-				const reader = new FileReader;
-				// Вешаем событие на читателя
-				reader.addEventListener('loadend', () => {
-					// Элемент списка .preview
-					const item = document.createElement('li');
-					// Картинка для предпросмотра
-					const image = new Image;
-					// URI картинки
-					image.src = `data:${file.type};base64,${btoa(reader.result)}`;
-					// Ссылка на исключение картинки из списка выгрузки
-					const remove = document.createElement('div');
-					// Элемент массива fileList
-					const fileItem = {
-						name: file.name,
-						modified: file.lastModified,
-						size: file.size,
-						data: reader.result
-					};
-					// Добавляем элемент в список выгрузки
-					fileList.push(fileItem);
-					// Обработчик клика по ссылке исключения картинки
-					remove.addEventListener('click', () => {
-						// Исключаем элемент с картинкой из списка выгрузки
-						fileList.splice(fileList.indexOf(fileItem), 1);
-						// Удаляем элемент списка (<li>) из <ul> 
-						item.classList.add('removing');
-						setTimeout(() => item.remove(), 100);
-					});
-					item.appendChild(remove);
-					item.appendChild(image);
-					preview.appendChild(item);
-				});
-				// Запускаем чтение файла
-				reader.readAsBinaryString(file);
-			});
-			// Сбрасываем значение <input>
-			input.value = '';
-			// Создаем клон <input>
-			const newInput = input.cloneNode(true);
-			// Заменяем <input> клоном
-			input.replaceWith(newInput);
-			// Теперь input будет указывать на клона
-			input = newInput;
-			// Повесим функцию onChange на событие change у нового <input>
-			input.addEventListener('change', onChange);
-		}
-	});
-}
-
 function openDropdown() {
-	jQuery('.open-link').on('click', function () {
+	jQuery('.open-link').on('click', function (e) {
+		e.preventDefault();
 		jQuery('.choose-menu').toggleClass('active');
 	});
 	$(document).mouseup(function (e) {
@@ -380,104 +315,6 @@ function openPro() {
 	});
 }
 
-function initSearchFormSwitcher() {
-	jQuery('.btn_search').on('click', function () {
-		jQuery('.search-holder').toggleClass('active');
-	});
-	$(document).mouseup(function (e) {
-		let div = $('.search-holder');
-		if (!div.is(e.target)
-			&& div.has(e.target).length === 0) {
-			div.removeClass('active');
-		}
-	});
-}
-
-function resizeBlock() {
-	if (window.matchMedia("screen and (max-width: 1319px)").matches) {
-		$('.btn_pay').appendTo($('.logo-area'));
-	}
-	else if (window.matchMedia("screen and (min-width: 1319px)").matches) {
-		$('.btn_pay').appendTo($('.header .btn-box'));
-	}
-}
-
-function loadMore() {
-	var $items = $(".category__item");
-	var count = 3;
-	var visible = 3;
-	var windowWidth = $(window).width();
-
-	function updateVisibility() {
-		if (windowWidth < 768) {
-			$items.slice(visible).hide();
-		} else {
-			$items.show();
-		}
-	}
-
-	updateVisibility();
-
-	$(window).resize(function () {
-		windowWidth = $(window).width();
-		updateVisibility();
-	});
-
-	$(".load_more").click(function (event) {
-		event.preventDefault();
-		$items.slice(visible, visible + count).show();
-		visible += count;
-
-		if (visible >= $items.length) {
-			$(this).hide();
-		}
-	});
-}
-
-$(window).resize(function () {
-	resizeBlock();
-	if ($(window).width() < 768) {
-		loadMore();
-	}
-
-
-});
-
-function subMenuNavigation() {
-	let doc = jQuery(document),
-		isTouchDevice = /Windows Phone/.test(navigator.userAgent) || ('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch;
-
-	if (isTouchDevice === true) {
-		jQuery('.nav .menu-item-has-children').each(function () {
-			let navItem = jQuery(this).addClass('touch-device'),
-				navItemLink = navItem.children('a'),
-				dataClick = navItem.attr('data-click', 0),
-				subMenu = navItem.children('.sub-menu');
-
-			navItemLink.on('touchstart', function (e) {
-				if (navItem.attr('data-click') == 0) {
-					e.preventDefault();
-
-					navItem.attr('data-click', 1).addClass('open');
-					subMenu.slideDown();
-				} else if (navItem.attr('data-click') == 1) {
-					navItem.attr('data-click', 0).removeClass('open');
-					subMenu.slideUp();
-				}
-			});
-
-			function outsideClickHandler(e) {
-				if (!jQuery(e.target).closest(navItem).length) {
-					navItem.removeClass('open').attr('data-click', 0);
-					subMenu.slideUp();
-				}
-			}
-
-			doc.on('click touchstart', outsideClickHandler);
-		});
-	}
-}
-
 function faqAccordeon() {
 	var allLi = jQuery('.faq-list li'),
 		allSub = allLi.children('.filter');
@@ -500,66 +337,6 @@ function faqAccordeon() {
 				allLi.not(itemParent).find('.text-faq').slideUp();
 				itemFilter.slideDown();
 				item.addClass('active');
-			}
-		});
-	});
-}
-
-function listCategory() {
-	var allLi = jQuery('.list-category .item'),
-		allSub = allLi.children('.sub-content');
-
-	jQuery('.list-category .item > .intro-box').each(function () {
-		var doc = jQuery(document),
-			$this = jQuery(this),
-			item = $this.parent('li'),
-			itemFilter = $this.next('.sub-content'),
-			itemParent = item.parents('li');
-
-		$this.on('click', function () {
-			if (jQuery(window).width() < 1024) {
-				if (item.hasClass('active')) {
-					itemFilter.slideUp();
-					item.removeClass('active');
-				} else {
-					allLi.not(itemParent).removeClass('active');
-					allLi.not(itemParent).find('.sub-content').slideUp();
-					itemFilter.slideDown();
-					item.addClass('active');
-				}
-			} else {
-				if (item.hasClass('active')) {
-					itemFilter.fadeOut();
-					item.removeClass('active');
-				} else {
-					allLi.not(itemParent).removeClass('active');
-					allLi.not(itemParent).find('.sub-content').fadeOut();
-					itemFilter.fadeIn();
-					item.addClass('active');
-				}
-			}
-		});
-	});
-}
-
-function tooltip() {
-	$('.how-list li').each(function () {
-		var tooltip = $(this).find('.tooltip');
-		var icon = $(this).find('.icon-info');
-		var close = $(this).find('.close');
-
-		// Открытие тултипа при клике на иконку
-		icon.click(function () {
-			tooltip.fadeIn();
-		});
-
-		// Закрытие тултипа при клике на крестик или вне области тултипа
-		$(document).mouseup(function (e) {
-			if (!tooltip.is(e.target) && tooltip.has(e.target).length === 0 && !icon.is(e.target)) {
-				tooltip.fadeOut();
-			}
-			if (close.is(e.target)) {
-				tooltip.fadeOut();
 			}
 		});
 	});
